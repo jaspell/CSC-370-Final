531ac8ba8a24dfb5259f7a65f3af6c2842e05ff2
Jackson Spell <jaspell@davidson.edu>
2015-04-22 15:27:14 -0400

    Took out Pixel usage.

--- color_image.py
+++ color_image.py
@@ -14,7 +14,7 @@ class ColorImage:
 	Color image object.
 	"""
 
-	def __init__(image_file=None, w=None, h=None):
+	def __init__(self, image_file=None, w=None, h=None):
 		"""
 		Image Constructor.
 
@@ -30,16 +30,15 @@ class ColorImage:
 		"""
 
 		if image_file is not None:
-			self.width, self.height, self.image = read_image(image_file)
+			self.width, self.height, self.image = self.read_image(image_file)
 
 		else:
 			self.width = w
 			self.height = h
 
-			self.image = [[pixel.Pixel(0, 0, 0) for j in range(self.width)]
-							for i in range(self.height)]
+			self.image = [[0 for j in range(3*self.width)] for i in range(self.height)]
 
-	def read_image(image_file):
+	def read_image(self, image_file):
 		"""
 		Read in image from PNG file.
 
@@ -47,7 +46,7 @@ class ColorImage:
 			image_file - string - path to file containing PNG image
 
 		Returns:
-			width, height, image as 2D array of Pixels 
+			width, height, image as 2D array of ints
 		"""
 
 		with open(image_file, 'r') as inf:
@@ -61,13 +60,13 @@ class ColorImage:
 
 			for line in pixels:
 				row = []
-				for i in range(width):
-					row.append(pixel.Pixel(line[i], line[i+1], line[i+2]))
+				for value in line:
+					row.append(value)
 				image.append(row)
 
 		return width, height, image
 
-	def write_to_file(filename):
+	def write_to_file(self, filename):
 		"""
 		Write image to file.
 
@@ -79,13 +78,6 @@ class ColorImage:
 		"""
 
 		output = []
-
-		# Convert image to a list of lists of ints
-		for line in self.image:
-			row = []
-			for pix in row:
-				row.append(pix.to_list())
-			output.append(row)
 
 		# Write output to file.
 		with open(filename, 'wb') as outf:
