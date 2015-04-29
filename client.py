05bc6a9f1d7872588c0836329f974b98da1d5f05
Jackson Spell <jaspell@davidson.edu>
2015-04-28 20:28:34 -0400

    Put region maker code into client.

--- client.py
+++ client.py
@@ -3,6 +3,7 @@
 """Client code for Final Project (Depth Image Segmentation)."""
 
 import os
+from random import randint
 
 import color_image
 import depth_image
@@ -25,6 +26,36 @@ def main():
 	#print len(image.image[0])
 
 	image.write_to_file(test_file)
+
+def create_segmented_image(regions, width, height):
+	"""
+	Create a ColorImage made from the segmented regions given.
+
+	Parameters:
+		regions - list of lists of tuples - segmented regions
+		width - int - width of image in pixels
+		height - int - height of image in pixels
+
+	Returns:
+		ColorImage of segmented regions
+	"""
+
+	segmented_image = color_image.ColorImage(width=width, height=height) 
+	#print segmented_image
+	
+	for region in regions:
+
+		#pick a random color for that entire region
+		rgb = [randint(0,255),randint(0,255),randint(0,255)]
+
+		for coord_tuple in region:
+
+			#Now go through and change the R, G, and B vals.
+			for i in range(0,3): 
+
+				segmented_image.image[coord_tuple[1]][3*coord_tuple[0]+i] = rgb[i]
+														  
+	return segmented_image
 
 
 if __name__ == "__main__":
