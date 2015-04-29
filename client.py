fdfbc264abd5e9e98141901b1220e58c024de385
Jackson Spell <jaspell@davidson.edu>
2015-04-28 21:24:35 -0400

    Got blur working

--- client.py
+++ client.py
@@ -3,6 +3,7 @@
 """Client code for Final Project (Depth Image Segmentation)."""
 
 import os
+import math
 from random import randint
 
 import color_image
@@ -21,11 +22,13 @@ def main():
 	#image = color_image.ColorImage(color_file)
 	#image = color_image.ColorImage(w=20, h=100)
 
-	image = depth_image.DepthImage(depth_file)
+	#image = depth_image.DepthImage(depth_file)
 
 	#print len(image.image[0])
 
-	image.write_to_file(test_file)
+	blur(color_image.ColorImage(color_file),10)
+
+	#image.write_to_file(test_file)
 
 def create_segmented_image(regions, width, height):
 	"""
@@ -68,6 +71,89 @@ def laplacian_segment(image):
 		list of lists of tuples - segmented regions
 	"""
 	pass
+
+def blur(image_original, radius):
+	"""
+	Blur image according to given radius.
+
+	Parameters:
+		image_original - ColorImage - original image
+		radius - int - radius around each pixel to blur
+
+	Returns:
+		ColorImage - blurred image
+	"""
+
+	image = image_original.image
+
+	total = get_total_for_filter(radius)    
+	
+	normFactor = 1.0/total
+	filt = make_filter(radius, normFactor)
+	
+	blurred = one_dimensional_blur(image_original, radius, filt, "horizontal")  
+
+	final_blurred= one_dimensional_blur(blurred, radius, filt, "vertical")
+
+	
+	final_blurred.write_to_file("test.png")         
+   
+
+def one_dimensional_blur(image_original, radius, filt, blur_mode):
+	blurred = color_image.ColorImage(width=image_original.width, height=image_original.height)
+	image = image_original.image
+	
+	for j, row in enumerate(image):
+		#make sure we are not at the boundary
+		if j > radius and j < (len(image)-radius):
+			for i,col in enumerate(row):
+				if i > radius and i < (len(row)-radius):
+					newRed, newGreen, newBlue = 0,0,0
+					for k in range(-radius+1, radius): #Now go through filter and apply blur horizontally
+						if blur_mode == "horizontal":
+							newRed = newRed + image[j][i+k][0]*filt[k+radius]
+							newGreen = newGreen + image[j][i+k][1]*filt[k+radius]
+							newBlue = newBlue + image[j][i+k][2]*filt[k+radius]
+						elif blur_mode == "vertical":
+							newRed = newRed + image[j+k][i][0]*filt[k+radius]
+							newGreen = newGreen + image[j+k][i][1]*filt[k+radius]
+							newBlue = newBlue + image[j+k][i][2]*filt[k+radius]       
+						else:
+							print "ERROR HAS OCCURED. Expected 'horizontal' or 'verticle' for blur mode but received ", blur_mode
+					#make new pixel as a tuple to be inserted.    
+					pixel = (int(newRed), int(newGreen), int(newBlue))
+					blurred.image[j][i] = pixel
+	return blurred
+
+def get_total_for_filter(radius):
+
+	total = 0
+
+	sigma = (radius - 1) / 3.0
+
+	for i in range(2*radius + 1):
+
+		x = i - radius
+		val = 1.0 / (sigma * math.sqrt(2.0 * math.pi)) * pow(math.e, (-pow(x, 2)
+															 / (2.0 * pow(sigma, 2))))
+		total += val
+
+	return total
+
+def make_filter(radius, normFactor):
+
+	filt = []
+
+	sigma = (radius - 1) / 3.0
+
+	for i in range(2*radius + 1):
+
+			x = i - radius
+			val = 1.0 / (sigma * math.sqrt(2.0 * math.pi)) * pow(math.e, (-pow(x, 2)
+																 / (2.0 * pow(sigma, 2))))
+			filt.append(val * normFactor)
+
+	return filt
 
 def gradient_segment(image, threshold):
 	"""
