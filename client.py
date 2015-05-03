bb8820e344bb06c795d2e5f7b451aedd784d55b6
Jackson Spell <jaspell@davidson.edu>
2015-05-03 11:20:24 -0400

    Reordered methods (semi-alphabetical now) and wrote laplacian segment and associated methods. Untested.

--- client.py
+++ client.py
@@ -5,6 +5,7 @@
 import os
 import math
 from random import randint
+import queue
 
 import color_image
 import depth_image
@@ -29,12 +30,105 @@ def main():
 	blurred.write_to_file("blur.png")
 
 	#print len(original.image[0])
+	
+	laplacian_segment(original, 200, 10, 20).write_to_file(test_file)
 
-	final = edge_detect(original, 10, 20)
+def gradient_segment(original, threshold):
+	"""
+	Segment the depth image using the gradient technique.
+
+	Parameters:
+		original - DepthImage - depth image to be segmented
+		threshold - float - maximum angle between two pixels in the same region
+
+	Returns:
+		list of lists of tuples - segmented regions
+	"""
+
+	pass
+
+def laplacian_segment(original, threshold, radius, scale):
+	"""
+	Segment the depth image using the Laplacian technique.
+
+	Parameters:
+		original - ColorImage or DepthImage - image to be segmented
+		threshold - int - 0 <= threshold < 256 - minimum value of edge between regions
+		radius - int - radius of blurring filter
+		scale - int - scaling factor for subtraction
+
+	Returns:
+		list of lists of tuples - segmented regions
+	"""
+
+	edged = edge_detect(original, radius, scale)
+					
+	# Create 2D boolean array, setting borders and detected edges to True.
+	added = [[True if original[i][j] > threshold 
+				   or i == 0 
+				   or i == original.height - 1 
+				   or j == 0 
+				   or j == original.width - 1 
+				   else False for j in range(original.width)] for i in range(original.height)]
+
+	regions = []
+
+	for i in range(original.height):
+		for j in range(original.width):
+			if not added[i][j]:
+				regions.append(group_region(added, i, j))
+
+	return regions
+
+def blur(original, radius):
+	"""
+	Blur image according to given radius.
+
+	Parameters:
+		original - ColorImage or DepthImage - original image
+		radius - int - radius around each pixel to blur
+		blur - bool - switch so that we can blur color and B&W images.
+
+	Returns:
+		ColorImage - blurred image
+	"""
+
+	filt = make_filter(radius)
+		
+	blurred = one_dimensional_blur(original, radius, filt, "horizontal")  
+	blurred = one_dimensional_blur(blurred, radius, filt, "vertical")
+
+	return blurred
+
+def create_segmented_image(regions, width, height):
+	"""
+	Create a ColorImage made from the segmented regions given.
+
+	Parameters:
+		regions - list of lists of tuples - segmented regions
+		width - int - width of image in pixels
+		height - int - height of image in pixels
+
+	Returns:
+		ColorImage of segmented regions
+	"""
+
+	segmented_image = color_image.ColorImage(width=width, height=height) 
+	#print segmented_image
 	
-	laplacian_segment(final, 200)
-	
-	final.write_to_file(test_file)
+	for region in regions:
+
+		#pick a random color for that entire region
+		rgb = [randint(0,255),randint(0,255),randint(0,255)]
+
+		for coord_tuple in region:
+
+			#Now go through and change the R, G, and B vals.
+			for i in range(0,3): 
+
+				segmented_image[coord_tuple[1]][3*coord_tuple[0]+i] = rgb[i]
+														  
+	return segmented_image
 
 def edge_detect(original, radius, scale):
 	"""
@@ -78,75 +172,91 @@ def edge_detect(original, radius, scale):
 			
 	return final
 
-def create_segmented_image(regions, width, height):
+def get_total_for_filter(radius):
 	"""
-	Create a ColorImage made from the segmented regions given.
+	Determine the filter's total.
 
 	Parameters:
-		regions - list of lists of tuples - segmented regions
-		width - int - width of image in pixels
-		height - int - height of image in pixels
-
-	Returns:
-		ColorImage of segmented regions
-	"""
-
-	segmented_image = color_image.ColorImage(width=width, height=height) 
-	#print segmented_image
-	
-	for region in regions:
-
-		#pick a random color for that entire region
-		rgb = [randint(0,255),randint(0,255),randint(0,255)]
-
-		for coord_tuple in region:
-
-			#Now go through and change the R, G, and B vals.
-			for i in range(0,3): 
-
-				segmented_image[coord_tuple[1]][3*coord_tuple[0]+i] = rgb[i]
-														  
-	return segmented_image
-
-def laplacian_segment(image, threshold):
-	"""
-	Segment the depth image using the Laplacian technique.
-
-	Parameters:
-		image - DepthImage - depth image to be segmented
-		threshold - int - 0 <= threshold < 256 - minimum value of edge between regions
-
-	Returns:
-		list of lists of tuples - segmented regions
-	"""
-	pass
-					
-	#subtracted
-
-	# Track the pixels that have been grouped.
-	#grouped = [[False for i in range(width)] for j in range(height)]
-
-	#while 
-
-def blur(original, radius):
-	"""
-	Blur image according to given radius.
-
-	Parameters:
-		original - ColorImage or DepthImage - original image
 		radius - int - radius around each pixel to blur
-		blur - bool - switch so that we can blur color and B&W images.
 
 	Returns:
-		ColorImage - blurred image
+		int - filter's total
 	"""
 
-	filt = make_filter(radius)
-		
-	blurred = one_dimensional_blur(original, radius, filt, "horizontal")  
-	blurred = one_dimensional_blur(blurred, radius, filt, "vertical")
+	total = 0
 
-	return blurred
+	sigma = (radius - 1) / 3.0
+
+	for i in range(2*radius + 1):
+
+		x = i - radius
+		val = 1.0 / (sigma * math.sqrt(2.0 * math.pi)) * pow(math.e, (-pow(x, 2)
+															 / (2.0 * pow(sigma, 2))))
+		total += val
+
+	return total
+
+def group_regions(added, i, j):
+	"""
+	Group pixels into a region via floodfill for Laplacian segmentation.
+
+	Parameters:
+		added - 2D boolean list - whether the pixel at i,j has been added to a region
+		i - int - height coordinate of starting pixel
+		j - int - width coordinate of starting pixel
+
+	Returns:
+		list of tuples - list of coordinate pairs for pixels in the region
+	"""
+
+	region = []
+	q = Queue.Queue()
+
+	# Start with the starting pixel in the queue
+	q.put((i, j))
+
+	while not q.empty():
+		x, y = q.get()
+
+		# Add edge-adjacent neighbors to the region if they have not been added.
+		for i in range(-1, 2):
+
+			if not added[x][y+i]:
+				q.put((x, y+i))
+				added[x][y+i] = True
+				region.append((x, y+i))
+
+			if not added[x+i][y]:
+				q.put((x+i, y))
+				added[x+i][y] = True
+				region.append((x+i, y))
+
+	return region
+
+def make_filter(radius):
+	"""
+	Make the filter for the blur.
+
+	Parameters:
+		radius - int - radius around each pixel to blur
+
+	Returns:
+		list of floats - blur filter
+	"""
+
+	filt = []
+
+	sigma = (radius - 1) / 3.0
+	normFactor = 1.0 / get_total_for_filter(radius)
+
+	for i in range(2*radius + 1):
+
+			x = i - radius
+			val = 1.0 / (sigma * math.sqrt(2.0 * math.pi)) * pow(math.e, (-pow(x, 2)
+																 / (2.0 * pow(sigma, 2))))
+			filt.append(val * normFactor)
+
+	return filt
 
 def one_dimensional_blur(original, radius, filt, blur_mode):
 	"""
@@ -212,69 +322,6 @@ def one_dimensional_blur(original, radius, filt, blur_mode):
 			blurred[j][i] = pixel
 
 	return blurred
-
-def get_total_for_filter(radius):
-	"""
-	Determine the filter's total.
-
-	Parameters:
-		radius - int - radius around each pixel to blur
-
-	Returns:
-		int - filter's total
-	"""
-
-	total = 0
-
-	sigma = (radius - 1) / 3.0
-
-	for i in range(2*radius + 1):
-
-		x = i - radius
-		val = 1.0 / (sigma * math.sqrt(2.0 * math.pi)) * pow(math.e, (-pow(x, 2)
-															 / (2.0 * pow(sigma, 2))))
-		total += val
-
-	return total
-
-def make_filter(radius):
-	"""
-	Make the filter for the blur.
-
-	Parameters:
-		radius - int - radius around each pixel to blur
-
-	Returns:
-		list of floats - blur filter
-	"""
-
-	filt = []
-
-	sigma = (radius - 1) / 3.0
-	normFactor = 1.0 / get_total_for_filter(radius)
-
-	for i in range(2*radius + 1):
-
-			x = i - radius
-			val = 1.0 / (sigma * math.sqrt(2.0 * math.pi)) * pow(math.e, (-pow(x, 2)
-																 / (2.0 * pow(sigma, 2))))
-			filt.append(val * normFactor)
-
-	return filt
-
-def gradient_segment(original, threshold):
-	"""
-	Segment the depth image using the gradient technique.
-
-	Parameters:
-		original - DepthImage - depth image to be segmented
-		threshold - float - maximum angle between two pixels in the same region
-
-	Returns:
-		list of lists of tuples - segmented regions
-	"""
-
-	pass
 
 if __name__ == "__main__":
 	main()
